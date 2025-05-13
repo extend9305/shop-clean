5741067d22130e69ab8569dd238b8af027535073
이동수 <dongsu.lee93@homeplus.co.kr>
2025-05-13 15:27:57 +0900

    Initial commit

---
+++ src/main/java/com/example/shopclean/ShopCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCleanApplication.class, args);
+    }
+
+}
